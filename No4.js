eae0da929fbfc6b9692e582fc00d112dfb449d5b
KazuoWatanabe <kuma.pappy777@gmail.com>
2021-10-18 22:56:57 +0900

    レッスン17

---
+++ No4.js
@@ -0,0 +1,10 @@
+function oshita() {
+    youso=document.getElementById('text01');
+    t=youso.value;
+
+    hyouji='こんにちは' + t + 'さん';
+    
+    gamenyouso = document.getElementById('gamen');
+    gamenyouso.innerHTML = hyouji;
+
+}
